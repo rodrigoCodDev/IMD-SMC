d200980636b2109d1a2866c006caeaf7f6f31057
rodrigoCodDev <rodrigo.oliveira200310@gmail.com>
2021-07-14 16:49:46 -0300

    Acrescentando Semana 6
    Criação do experimento da Semana 6.

---
+++ Semana 6/Experimento 1/experimento1.c
@@ -0,0 +1,64 @@
+// PIC18F45K20
+
+#pragma config FOSC = INTIO67   // Oscilador
+#pragma config BOREN = OFF      // Resetar o microcontrolador quando houver queda de tensão.
+#pragma config PBADEN = OFF     // Desligando conversor AC/DC de PORTB
+
+#include <xc.h>
+#define _XTAL_FREQ 1000000
+
+void main(void) {
+    // Configurando saída no PORTD
+    TRISD = 0b0000000; 
+    PORTD = 0b00000011;
+    
+    // Configurando entrada com interrupção
+    TRISB = 0b00000001;         // Configura o bit RB0 como entrada.
+    INTCONbits.INT0IE = 1;      // Habilitar interrupção externa INT0
+    INTCONbits.INT0IF = 0;      // Limpar flag da interrupção INT0
+    INTCON2bits.INTEDG0 = 0;    // Habilitar interrupção INT0 na borda de descida
+    RCONbits.IPEN = 0;          // Desligar todas as prioridades interrupção
+    INTCONbits.GIE = 1;         // Habilitar interrupção geral.     
+    
+    while(1) {
+        __delay_ms(70);
+        PORTD = (PORTD << 1);
+        
+        if(PORTD == 0b10000000) {
+            PORTD = 0b10000001;
+        }
+        
+        if(PORTD == 0b00000010) {
+            PORTD = 0b00000011;
+        }
+    }
+    
+    return;
+}
+
+
+void __interrupt() funcao(){  
+    
+    if (INTCONbits.INT0IF == 1) {
+        INTCONbits.INT0IF = 0;
+        
+        if(PORTD == 0b00011000) {
+            // Se a sequência selecionada foi correta, faça...
+            int tempo = 0;
+            
+            // Pisque todos os LEDs PORTD durante 2 segundos
+            while(tempo < 2000) {
+                PORTD = 0b00000000;
+                __delay_ms(100);
+                
+                PORTD = 0b11111111;
+                __delay_ms(100);
+                
+                tempo += 200;
+            }
+            
+            // Reconfigure a saída, reinicializando o jogo
+            PORTD = 0b00000011;
+        }
+    }
+}
